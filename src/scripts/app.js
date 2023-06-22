b740e168450447a280d4a2285a73862c30b6b091
Galih Tripambudi <87571952+testraven11111@users.noreply.github.com>
2023-06-22 20:00:05 +0700

    Add files via upload

---
+++ src/scripts/app.js
@@ -0,0 +1,39 @@
+import UrlParser from './routes/url-parser';
+import routes from './routes/routes';
+
+class App {
+    constructor({ content, menu, drawer, main }) {
+        this._content = content;
+        this._menu = menu;
+        this._drawer = drawer;
+        this._main = main;
+        this.onInit();
+    }
+
+    onInit() {
+        this._menu.addEventListener('click', (event) => {
+            this._drawer.classList.toggle('open');
+            event.stopPropagation();
+        });
+        this._main.addEventListener('click', () => {
+            this._drawer.classList.remove('open');
+        });
+    }
+
+    async renderPage() {
+        const url = UrlParser.parseActiveUrlWithCombiner();
+        const page = routes[url];
+
+        try {
+            this._content.innerHTML = await page.render();
+            await page.afterRender();
+        } catch (error) {
+            this._content.innerHTML = '<error-message></error-message>';
+            console.error(error);
+        }
+
+        this._drawer.classList.remove('open');
+    }
+}
+
+export default App;
